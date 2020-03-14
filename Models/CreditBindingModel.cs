182a604fa3e03b22000934764f65a7e07db69cac
Дмитрий <rabolan@mail.ru>
2020-03-15 00:15:34 +0300

    1

--- Models/CreditBindingModel.cs
+++ Models/CreditBindingModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace СreditСalculator.Models
         /// <summary>
         /// Сумма кредита
         /// </summary>
+        [UIHint("Сумма кредита")]
         public double SummaCredit { get; set; }
         /// <summary>
         /// Срок кредитования
