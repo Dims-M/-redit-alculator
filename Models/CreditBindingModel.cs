9b7d68333db7b2ace5fdd027053346c2a5152c79
Dims-M <Dimsam2014@yandex.ru>
2020-03-17 13:36:04 +0300

    Add files via upload

--- Models/CreditBindingModel.cs
+++ Models/CreditBindingModel.cs
@@ -14,26 +14,34 @@ namespace СreditСalculator.Models
         /// <summary>
         /// Сумма кредита
         /// </summary>
+        [Required(ErrorMessage = "Сумма кредита не должна быть пустой. И быть больше 1 мл. руб")] //Поле обязателько к заполнению.
+        [StringLength(7)]
         [UIHint("SummaCredit")]
         [Display(Name= "Сумма кредита")]
         public double SummaCredit { get; set; }
-      
+
         /// <summary>
         /// Срок кредитования
         /// </summary>
+        [Required(ErrorMessage = "Укажите нужный срок кредита")]
+        [StringLength(2)]
         [UIHint("TermCredit")]
         [Display(Name = "Срок кредитования")]
+      
         public int TermCredit { get; set; }
         /// <summary>
         /// Ставка кредитования 
         /// </summary>
         [UIHint("LendingTate")]
+        [Required(ErrorMessage = "Укажите Ставку кредитования")]
+        [StringLength(2)]
         [Display(Name = "Ставка кредитования ")]
         public int LendingTate { get; set; }
 
         /// <summary>
         /// Принятие соглашеения на обработку персональных данных
         /// </summary>
+        [Required]
         [UIHint("AdoptionAgreement")]
         public bool AdoptionAgreement { get; set; }
 
